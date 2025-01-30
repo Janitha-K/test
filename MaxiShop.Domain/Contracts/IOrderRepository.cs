91e4e31ac021c07e367eee86c5f9e9c1060e63cc
Subbulakshmi3107 <21ubosubbulakshmi@gmail.com>
2025-01-30 14:07:22 +0530

    order repo created

---
+++ MaxiShop.Domain/Contracts/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxiShop.Domain.Contracts
+{
+    internal interface IOrderRepository
+    {
+    }
+}
